87699eb91565537e65d7cf124845709cbe6355c4
yokonoji <yokonoji.work@gmail.com>
2019-09-28 12:00:01 +0900

    const component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
 
@@ -10,15 +11,29 @@ import logo from './logo.svg';
 //   );
 // }
 
-class App extends Component {
-  render(){
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("clickclick")}} />
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render(){
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("clickclick")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
