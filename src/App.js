49c44b8f00209503b7ea92616deca185f25a756c
yokonoji <yokonoji.work@gmail.com>
2019-09-28 09:52:00 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 
-function App() {
-  return (
-    <div>
-      Hello React
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       Hello React
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      "h1",
+      null,
+      "Hello, React2"
+    );
+  }
 }
 
 export default App;
